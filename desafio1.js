ced54273ea53879c2a42295661f9b64ec8c19869
Shirley-Aguilar-Aguilar <shirley.aguilar.agui@gmail.com>
2022-02-15 21:51:44 -0500

    enviando proyecto hojas css,html y js

--- desafio1.js
+++ desafio1.js
@@ -7,7 +7,7 @@ let encriptedText = "";
 
 function encriptarTexto(){
     let textFromUI= document.getElementById("inputUsuario").value;
-    console.log("mi textFromUI es:: " + textFromUI);
+    console.log("mi textFromUI es: " + textFromUI);
     let output = "";
 
     for(let i=0; i < textFromUI.length; i++)
@@ -35,20 +35,54 @@ function encriptarTexto(){
         }          
     }
 
-    console.log("mi output es:: " + output);
+    console.log("mi output es: " + output);
           
     document.getElementById("inputEncriptado").value = output;
 
     encriptedText = output;
+    return output;
 
 }
-
+let encriptarButton =document.getElementById("encriptarButton");
+encriptarButton.addEventListener("click", encriptarTexto);
 
 function textToClipboard() {
-    var dummy = document.createElement("textarea");;
+    var dummy = document.createElement("textarea");
     document.body.appendChild(dummy);
     dummy.value = encriptedText;
     dummy.select();
     document.execCommand("copy");
     document.body.removeChild(dummy);
 }
+let copyButton =document.getElementById("copyButton");
+copyButton.addEventListener("click", textToClipboard);
+
+
+function desencriptarTexto(){
+    let wordInput= document.getElementById("inputUsuario").value;
+    function replaceAll(string, search, replace) {
+        return string.split(search).join(replace);
+        }
+        let changeA= replaceAll(wordInput, 'ai', 'a') ;
+        let changeI= replaceAll(changeA, 'imes', 'i') ;
+        let changeE= replaceAll(changeI, 'enter', 'e') ;
+         let changeO=replaceAll(changeE, 'ober', 'o') ; 
+        let finalWord= replaceAll(changeO, 'ufat', 'u') ;
+        
+        document.getElementById("inputUsuario").value= finalWord ;
+        console.log(finalWord);
+        
+       
+    return finalWord ;
+
+}
+let desencriptarButton =document.getElementById("desencriptarButton");
+desencriptarButton.addEventListener("click", desencriptarTexto);
+
+    
+    
+   
+
+
+
+
